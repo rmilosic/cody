3f237a71ca0cb2552e1d31faf83292dae2afaf9e
Tat Dat Duong <david@duong.cz>
2025-04-12 14:14:35 +0200

    Add pickled data, serve debug endpoint for viewing data outside

---
+++ src/agent/http/debug.py
@@ -0,0 +1,77 @@
+# ruff: noqa: D100, D101, D103
+
+import datetime
+
+import pandas as pd
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+zpravy = pd.read_pickle("pickle/zpravy.pkl")
+vazby = pd.read_pickle("pickle/vazby.pkl")
+dokumentace = pd.read_pickle("pickle/dokumentace.pkl")
+material = pd.read_pickle("pickle/material.pkl")
+vykony = pd.read_pickle("pickle/vykony.pkl")
+vykpac = pd.read_pickle("pickle/vykpac.pkl")
+
+
+class PatientDataRequest(BaseModel):
+    iloc: int
+
+
+@app.get("/get_patient_data/{iloc}")
+async def get_patient_data(iloc: int):
+    try:
+        item_zpravy = zpravy.iloc[iloc]
+    except IndexError:
+        raise HTTPException(status_code=404, detail="Patient data not found")
+
+    item_vazby = vazby[
+        (vazby["SERIAL"] == item_zpravy["SERIAL"])
+        & (vazby["AMBNUM"] == item_zpravy["AMBNUM"])
+    ]
+
+    item_vazby_dokumentace_date = datetime.datetime.strftime(
+        pd.to_datetime(item_vazby["DATFR"], format="%d.%m.%Y %H:%M").squeeze(),
+        "%Y%m%d_%H%M00",
+    )
+    item_vazby_vyk_date = datetime.datetime.strftime(
+        pd.to_datetime(item_vazby["DATFR"], format="%d.%m.%Y %H:%M").squeeze(),
+        "%d.%m.%Y 00:00",
+    )
+
+    item_dokumentace = dokumentace[
+        (dokumentace["AMBNUM"] == item_zpravy["AMBNUM"])
+        & (dokumentace["DATUM_CAS"] == item_vazby_dokumentace_date)
+    ]
+
+    cispac = next(x for x in item_dokumentace["CISPAC"])
+
+    material_data = material[
+        (material["CISPAC"] == cispac) & (material["DATUM"] == item_vazby_vyk_date)
+    ]
+    vykony_data = vykony[
+        (vykony["CISPAC"] == cispac) & (vykony["DATUM"] == item_vazby_vyk_date)
+    ]
+    vykpac_data = vykpac[vykpac["CISPAC"] == cispac]
+
+    response = {
+        "zpravy_content": item_zpravy["CONTENT"],
+        "vazby": item_vazby.fillna("").to_dict(orient="records"),
+        "dokumentace": item_dokumentace.fillna("").to_dict(orient="records"),
+        "material": material_data.fillna("").to_dict(orient="records"),
+        "vykony": vykony_data.fillna("").to_dict(orient="records"),
+        "vykpac": vykpac_data.fillna("").to_dict(orient="records"),
+    }
+
+    return response
