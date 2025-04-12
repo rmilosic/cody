3ca02accd596d52e07ae1c0573a15fddcdc2bff6
Tat Dat Duong <david@duong.cz>
2025-04-12 20:31:09 +0200

    Add eval

--- tests/evals.py
+++ tests/evals.py
@@ -9,19 +9,37 @@ from agent.graph import graph
 load_dotenv()
 
 
-def correct(outputs: dict, reference_outputs: dict) -> bool:
-    expected_vykony = json.loads(reference_outputs["vykony"])
-    expected_materialy = json.loads(reference_outputs["materialy"])
+def accuracy(outputs: dict, reference_outputs: dict) -> float:
+    human_vykony = json.loads(reference_outputs["vykony"])
+    llm_vykony = (outputs.get("diagnosis") or {}).get("vykony") or []
 
-    actual_vykony = outputs.get("vykony")
-    actual_materialy = outputs.get("materialy")
+    human_kody = set(v["kod_vykonu"] for v in human_vykony)
+    llm_kody = set(v["code"] for v in llm_vykony)
 
-    return actual_vykony == expected_vykony and actual_materialy == expected_materialy
+    true_positives = human_kody & llm_kody
+
+    precision = len(true_positives) / len(llm_kody) if len(llm_kody) != 0 else 0.0
+    recall = len(true_positives) / len(human_kody) if len(human_kody) != 0 else 0.0
+
+    f1 = (
+        2 * precision * recall / (precision + recall)
+        if precision + recall != 0.0
+        else 0.0
+    )
+
+    return [
+        {"score": precision, "key": "precision"},
+        {"score": recall, "key": "recall"},
+        {"score": f1, "key": "f1"},
+    ]
 
 
 async def main():
     await aevaluate(
-        graph, data="rakathon-oncoders", evaluators=[correct], max_concurrency=20
+        graph,
+        data="rakathon-oncoders",
+        evaluators=[accuracy],
+        max_concurrency=20,
     )
 
 
