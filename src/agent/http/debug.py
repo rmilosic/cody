6a61c6bfca0464dbe5570084fc6e5804c8c2b273
Tat Dat Duong <david@duong.cz>
2025-04-12 17:28:26 +0200

    Use Duckdb as source of truth for debug

--- src/agent/http/debug.py
+++ src/agent/http/debug.py
@@ -1,9 +1,9 @@
-# ruff: noqa: D100, D101, D103
+# ruff: noqa: D100, D101, D103, F841
 
-import datetime
 
+import duckdb
 import pandas as pd
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -17,56 +17,164 @@ app.add_middleware(
 )
 
 
+zpravy = pd.read_pickle("pickle/zpravy.pkl")
+vazby = pd.read_pickle("pickle/vazby.pkl")
+dokumentace = pd.read_pickle("pickle/dokumentace.pkl")
+materialy = pd.read_pickle("pickle/material.pkl")
+vykony = pd.read_pickle("pickle/vykony.pkl")
+vykpac = pd.read_pickle("pickle/vykpac.pkl")
+
+
+def read_jsonl(path: str) -> dict:
+    import json
+
+    with open(path) as f:
+        obj_lines = [json.loads(line) for line in f]
+        return {str(line["code"]): line["name"] for line in obj_lines}
+
+
+materialy_labels = read_jsonl("data/ciselniky/materialy.jsonl")
+vykony_labels = read_jsonl("data/ciselniky/vykon.jsonl")
+
+
+res = duckdb.sql(
+    """
+    with zpravy_a_vazby as (
+        select
+            z.serial as zprava_serial,
+            z.rc,
+            z.ambnum,
+            z.content,
+            date_trunc('day', STRFTIME(STRPTIME(vazby.DATFR, '%d.%m.%Y %H:%M'), '%Y-%m-%dT%H:%M:%S')::timestamp) as datum_a_cas_zpravy,
+            vazby.DEPARTM,
+            vazby.WHO,
+        from zpravy z
+            left join vazby using (serial, ambnum)
+    ), zpravy_a_vazby_a_dokumentace_tmp as (
+        select distinct  -- why do we have duplicates
+            datum_a_cas_zpravy,
+            date_trunc('day', strptime(d.DATUM_CAS::text, '%Y%m%d_%H%M%S')) as datum_a_cas_dokumentace,
+            zv.ambnum,
+            zv.content,
+            d.CISPAC,
+        from zpravy_a_vazby zv
+            asof left join dokumentace d
+                on zv.ambnum = d.ambnum
+                and date_trunc('day', strptime(d.DATUM_CAS::text, '%Y%m%d_%H%M%S')) >= datum_a_cas_zpravy
+    ), zpravy_a_vazby_a_dokumentace as (
+        select
+            datum_a_cas_zpravy,
+            ambnum,
+            array_agg(distinct content) as contents,
+            min(cispac) as cispac,
+        from zpravy_a_vazby_a_dokumentace_tmp
+        group by 
+            datum_a_cas_zpravy,
+            ambnum
+    ), zpravy_a_vazby_a_dokumentace_a_vykony_tmp as (
+        select
+            date_trunc('day', strptime(datum::text, '%d.%m.%Y %H:%M')) as datum_vykonu,
+            z.*,
+            u.CDOKL,
+            u.kod as kod_vykonu,
+            u.odbornost,
+            u.mnozstvi as mnozstvi_vykonu,
+            u.body,
+        from zpravy_a_vazby_a_dokumentace z
+            left join vykony u on
+                z.cispac = u.cispac
+                and date_trunc('day', strptime(datum::text, '%d.%m.%Y %H:%M')) = datum_a_cas_zpravy
+    ), zpravy_a_vazby_a_dokumentace_a_vykony as (
+        select
+            datum_a_cas_zpravy,
+            contents,
+            AMBNUM,
+            cispac,
+            array_agg(
+                struct_pack(
+                    CDOKL,
+                    kod_vykonu,
+                    odbornost,
+                    mnozstvi_vykonu,
+                    body
+                )
+            ) as vykony
+        from zpravy_a_vazby_a_dokumentace_a_vykony_tmp
+        group by 
+            datum_a_cas_zpravy,
+            contents,
+            AMBNUM,
+            cispac,
+    ), zpravy_a_vazby_a_dokumentace_a_vykony_a_materialy_tmp as (
+        select
+            z.*,
+            m.cdokl,
+            m.kod as kod_materialu,
+            m.mnozstvi as mnozstvi_materialu,
+        from zpravy_a_vazby_a_dokumentace_a_vykony z
+            left join materialy m on
+                z.cispac = m.cispac
+                and date_trunc('day', strptime(m.datum::text, '%d.%m.%Y %H:%M')) = datum_a_cas_zpravy
+    ), zpravy_a_vazby_a_dokumentace_a_vykony_a_materialy as (
+        select
+            datum_a_cas_zpravy,
+            contents,
+            AMBNUM,
+            cispac,
+            vykony,
+            array_agg(
+                struct_pack(
+                    cdokl,
+                    kod_materialu,
+                    mnozstvi_materialu
+                )
+            ) as materialy
+        from zpravy_a_vazby_a_dokumentace_a_vykony_a_materialy_tmp
+        group by 
+            datum_a_cas_zpravy,
+            contents,
+            AMBNUM,
+            cispac,
+            vykony,
+    )
+    select *
+    from zpravy_a_vazby_a_dokumentace_a_vykony_a_materialy
+    order by datum_a_cas_zpravy ASC
+    """
+).df()
+
+
 @app.get("/get_patient_data/{iloc}")
 async def get_patient_data(iloc: int):
-    zpravy = pd.read_pickle("pickle/zpravy.pkl")
-    vazby = pd.read_pickle("pickle/vazby.pkl")
-    dokumentace = pd.read_pickle("pickle/dokumentace.pkl")
-    material = pd.read_pickle("pickle/material.pkl")
-    vykony = pd.read_pickle("pickle/vykony.pkl")
-    vykpac = pd.read_pickle("pickle/vykpac.pkl")
+    row = res.iloc[iloc].fillna("").to_dict()
 
-    try:
-        item_zpravy = zpravy.iloc[iloc]
-    except IndexError:
-        raise HTTPException(status_code=404, detail="Patient data not found")
+    def map_kod_vykonu(input: dict) -> dict:
+        res = input.copy()
+        if kod_vykonu := vykony_labels.get(str(res["kod_vykonu"])):
+            res["kod_vykonu"] = f"({res['kod_vykonu']}): {kod_vykonu}"
+        return res
 
-    item_vazby = vazby[
-        (vazby["SERIAL"] == item_zpravy["SERIAL"])
-        & (vazby["AMBNUM"] == item_zpravy["AMBNUM"])
-    ]
-
-    item_vazby_dokumentace_date = datetime.datetime.strftime(
-        pd.to_datetime(item_vazby["DATFR"], format="%d.%m.%Y %H:%M").squeeze(),
-        "%Y%m%d_%H%M00",
-    )
-    item_vazby_vyk_date = datetime.datetime.strftime(
-        pd.to_datetime(item_vazby["DATFR"], format="%d.%m.%Y %H:%M").squeeze(),
-        "%d.%m.%Y 00:00",
-    )
-
-    item_dokumentace = dokumentace[
-        (dokumentace["AMBNUM"] == item_zpravy["AMBNUM"])
-        & (dokumentace["DATUM_CAS"] == item_vazby_dokumentace_date)
-    ]
-
-    cispac = next(x for x in item_dokumentace["CISPAC"])
-
-    material_data = material[
-        (material["CISPAC"] == cispac) & (material["DATUM"] == item_vazby_vyk_date)
-    ]
-    vykony_data = vykony[
-        (vykony["CISPAC"] == cispac) & (vykony["DATUM"] == item_vazby_vyk_date)
-    ]
-    vykpac_data = vykpac[vykpac["CISPAC"] == cispac]
+    def map_kod_materialu(input: dict) -> dict:
+        res = input.copy()
+        if kod_materialu := (
+            materialy_labels.get(str(int(res["kod_materialu"])))
+            or materialy_labels.get(str(res["kod_materialu"]))
+        ):
+            res["kod_materialu"] = f"({res['kod_materialu']}): {kod_materialu}"
+        return res
 
     response = {
-        "zpravy_content": item_zpravy["CONTENT"],
-        "vazby": item_vazby.fillna("").to_dict(orient="records"),
-        "dokumentace": item_dokumentace.fillna("").to_dict(orient="records"),
-        "material": material_data.fillna("").to_dict(orient="records"),
-        "vykony": vykony_data.fillna("").to_dict(orient="records"),
-        "vykpac": vykpac_data.fillna("").to_dict(orient="records"),
+        "zpravy_content": "\n".join(row["contents"]),
+        "material": list(
+            map_kod_materialu(x)
+            for x in row["materialy"]
+            if any(filter(lambda y: y, x.values()))
+        ),
+        "vykony": list(
+            map_kod_vykonu(x)
+            for x in row["vykony"]
+            if any(filter(lambda y: y, x.values()))
+        ),
     }
 
     return response
